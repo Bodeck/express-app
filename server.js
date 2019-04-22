8897602b313964d06007c184c5413a9372ccdd8c
Bogus Franczak <bfranczak@gmail.com>
2019-04-22 15:50:17 +0200

    Add middleware function for store

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.use(express.static('assets'));
+app.use(express.static('assets'),
+  (req, res, next) => {
+    if(req.url === '/store') {
+      console.log(`Jestem posrednikiem w zadaniu do /store`);
+    };
+    next();
+  });
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.sendFile('/index.html');
 });
 
@@ -15,8 +21,16 @@ app.get('/userform', (req, res) => {
   res.end(JSON.stringify(response));
 });
 
-const server = app.listen(port,'localhost', () => {
+app.get('/store', (req, res) => {
+  res.send(`This is store`);
+});
+
+const server = app.listen(port, 'localhost', () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`Przykladowa aplikacja nasluchuje na http://${host}:${port}`);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send(`We could not found what you are requesting.`)
 });
